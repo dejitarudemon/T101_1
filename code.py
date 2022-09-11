876cdcd5e5b6b8bc0f558193842e602b2c9500a1
dejitarudemon <vlad.vir282@yandex.ru>
2022-09-11 17:22:32 +0300

    update chcecking_facts function

--- code.py
+++ code.py
@@ -1,6 +1,3 @@
-from lab1 import generate_simple_rules, generate_rand_facts
-
-
 def parse_rules(rules) -> dict:
     result = {'and': [], 'or': [], 'not': []}
     for rule in rules:
@@ -19,31 +16,26 @@ def check_validate_rules(rules) -> dict:
         rule = rules[oper]
         for i in range(0, len(rule) - 1):
             for j in range(i + 1, len(rule)):
-                if rule[i][0] == rule[j][0]:
+                if rule[i][0] == rule[j][0] and rule[i][1] != rule[j][1]:
                     rule[i][1] = rule[j][1] = ['Unknown']
 
     return rules
 
 
-def check_facts(rules, facts) -> dict:
-    result = {}
-    for i, oper in enumerate(rules):
-        if oper == 'or':
-            result[i] = any([z in facts for z in rule[1]])
-
-        if rule[0] == 'and':
-            result[i] = all([z in facts for z in rule[1]])
-
-        if rule[0] == 'not':
-            result[i] = not any([z in facts for z in rule[1]])
+def check_facts(rules, facts) -> list:
+    result = []
+    opers = ('and', 'or', 'not')
+    for oper in opers:
+        rule = rules[oper]
+        for r in rule:
+            if oper == 'or':
+                if any([z in facts for z in r[0]]):
+                    result.append(r[1])
+            if oper == 'and':
+                if all([z in facts for z in r[0]]):
+                    result.append(r[1])
+            if oper == 'not':
+                if not any([z in facts for z in r[0]]):
+                    result.append(r[1])
 
     return result
-
-
-a = generate_simple_rules(10, 4, 4)
-f = generate_rand_facts(10, 10)
-b = parse_rules(a)
-print(a)
-print(b)
-print(f)
-# print(check_facts(b, f))
