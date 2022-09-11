eec5da67449a8fd8893f9d48818ea0c85c4f8370
dejitarudemon <vlad.vir282@yandex.ru>
2022-09-11 16:25:24 +0300

    update checking function

--- code.py
+++ code.py
@@ -1,11 +1,13 @@
 from lab1 import generate_simple_rules, generate_rand_facts
+
+
 def parse_rules(rules) -> list:
     result = []
     for rule in rules:
         for oper in ('or', 'and', 'not'):
             incoming = rule['if'].get(oper)
             if incoming:
-                result.append([oper, set(incoming), rule['then']])
+                result.append([oper, incoming, rule['then']])
                 break
 
     result.sort()
@@ -16,14 +18,16 @@ def check_facts(rules, facts) -> dict:
     result = {}
     for i, rule in enumerate(rules):
         if rule[0] == 'or':
-            result[i] = False
-            for num in rule[1]:
-                result[i] = result[i] or num in facts
+            result[i] = any([z in facts for z in rule[1]])
 
         if rule[0] == 'and':
-            result[i] = rule[1].issubset(facts)
+            result[i] = all([z in facts for z in rule[1]])
+
+        if rule[0] == 'not':
+            result[i] = not all([z in facts for z in rule[1]])
 
     return result
+
 
 a = generate_simple_rules(10, 4, 4)
 f = generate_rand_facts(10, 10)
